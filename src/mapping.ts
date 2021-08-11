f649de4d708de0b09e05d6fd94f04953d159c202
Teuul <teulery@gmail.com>
2021-08-11 17:36:54 +0200

    first commit: celo governance smart-contract event indexer

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,35 @@
-import { Exchanged} from '../generated/Exchange/Exchange';
+import {
+	ApproverSet,
+	ConstitutionSet,
+	ProposalQueued,
+	ProposalUpvoted,
+	ProposalUpvoteRevoked,
+	ProposalVoted
+} from '../generated/Governance/Governance';
 import { Event, Field, ArrayField, Item } from '../generated/schema';
-import { log, ethereum } from '@graphprotocol/graph-ts';
+import { ethereum } from '@graphprotocol/graph-ts';
 import { handleEvent } from './utils/utils';
 
-export function handleNewExchange(event: Exchanged): void {
-  handleEvent(event,"Exchanged");
+export function handleApproverSet(event: ApproverSet): void {
+  handleEvent(event,"ApproverSet");
+}
+
+export function handleConstitutionSet(event: ConstitutionSet): void {
+  handleEvent(event,"ConstitutionSet");
+}
+
+export function handleProposalQueued(event: ProposalQueued): void {
+  handleEvent(event,"ProposalQueued");
+}
+
+export function handleProposalUpvoted(event: ProposalUpvoted): void {
+  handleEvent(event,"ProposalUpvoted");
+}
+
+export function handleProposalUpvoteRevoked(event: ProposalUpvoteRevoked): void {
+  handleEvent(event,"ProposalUpvoteRevoked");
+}
+
+export function handleProposalVoted(event: ProposalVoted): void {
+  handleEvent(event,"ProposalVoted");
 }
